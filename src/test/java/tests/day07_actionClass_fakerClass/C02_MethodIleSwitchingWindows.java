a18e7e3a26b5575fa0cdcde74a250b01b6ef8c57
bulutluoz <abbulutluoz@gmail.com>
2024-03-27 10:40:29 +0100

    7.gun sonu

--- src/test/java/tests/day07_actionClass_fakerClass/C02_MethodIleSwitchingWindows.java
+++ src/test/java/tests/day07_actionClass_fakerClass/C02_MethodIleSwitchingWindows.java
@@ -1,17 +1,35 @@
 package tests.day07_actionClass_fakerClass;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import utilities.ReusableMethods;
+import utilities.TestBase_BeforeAfter;
 
-public class C02_MethodIleSwitchingWindows {
+public class C02_MethodIleSwitchingWindows extends TestBase_BeforeAfter {
 
     @Test
     public void windowtesti(){
 
         //● https://testotomasyonu.com/addremove/ adresine gidin.
 
+        driver.get("https://testotomasyonu.com/addremove/");
+
         //● ’Please click for Electronics Products’ linkine tiklayin.
+
+        driver.findElement(By.linkText("Electronics Products"))
+                .click();
 
         //● Electronics sayfasinin acildigini test edin
 
+        // once yeni window'a gecis yapalim
+        ReusableMethods.switchWindow(driver,"https://testotomasyonu.com/category/7/products");
+
+        String expectedTitleIcerik = "Electronics";
+        String actualTitle = driver.getTitle();
+
+        Assert.assertTrue(actualTitle.contains(expectedTitleIcerik));
+
+        ReusableMethods.bekle(2);
     }
 }
--- src/test/java/utilities/ReusableMethods.java
+++ src/test/java/utilities/ReusableMethods.java
@@ -1,9 +1,11 @@
 package utilities;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class ReusableMethods {
 
@@ -29,5 +31,30 @@ public class ReusableMethods {
 
 
         return donusturulenList;
+    }
+
+    public static void switchWindow(WebDriver driver, String hedefUrl){
+
+        // birden fazla window acik iken bu method calisacak
+
+        // 1- acik olan tum window'larin WHD'lerini bir Set olarak kaydedelim
+
+        Set<String> tumWHDSeti = driver.getWindowHandles();
+
+        // 2- bir for-each loop ile, her bir WHD'ini kullanip
+        //    o window'a gidelim, eger gittigimiz window'da Url hedefUrl'e esit ise
+        //    loop'u bitirelim
+
+        for (String eachWHD : tumWHDSeti
+             ) {
+
+            driver.switchTo().window(eachWHD);
+
+            if (driver.getCurrentUrl().equals(hedefUrl)){
+                break;
+            }
+
+        }
+
     }
 }
