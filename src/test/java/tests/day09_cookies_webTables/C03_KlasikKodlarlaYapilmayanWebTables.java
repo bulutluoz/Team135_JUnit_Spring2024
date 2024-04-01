6057bbb5b72c2d24fb1151b10ad4e738310657b1
bulutluoz <abbulutluoz@gmail.com>
2024-04-01 12:21:18 +0200

    day09 sonu

--- src/test/java/tests/day09_cookies_webTables/C03_KlasikKodlarlaYapilmayanWebTables.java
+++ src/test/java/tests/day09_cookies_webTables/C03_KlasikKodlarlaYapilmayanWebTables.java
@@ -52,12 +52,53 @@ public class C03_KlasikKodlarlaYapilmayanWebTables extends TestBase_BeforeAfter 
 
         //  8. Tablodaki 3.kolonu yazdirin
 
+        List<WebElement> ucuncuSutunDataList = driver.findElements(By.xpath("(//div[@role='trow'])/div[@role='tdata'][3]"));
+        System.out.println("ucuncu sutun elementleri list : " +
+                            ReusableMethods.stringListeDonustur(ucuncuSutunDataList));
 
 
         //  9. Tabloda " Category" si Furniture olan urunun fiyatini yazdirin
+        
+        // herbir satiri elden gecirip
+        // Cotegory bilgisi Furniture olan satiri bulup
+        // o satirdaki urun fiyatini yazdirmaliyiz
+        // her satirdaki Category elementinin locate'i
+        // (//div[@role='trow'])[    5     ]/div[@role='tdata'][2]
 
+        for (int i = 1; i <= satirElementleriList.size() ; i++) {
+
+            String dinamikCategoryXpathi = "(//div[@role='trow'])[" + i + "]/div[@role='tdata'][2]";
+            String dinamikFiyatXpathi = "(//div[@role='trow'])[" + i + "]/div[@role='tdata'][3]";
+
+
+            String satirdakiCategoryDegeri = driver.findElement(By.xpath(dinamikCategoryXpathi))
+                                                    .getText();
+            String satirdakiFiyatDegeri = driver.findElement(By.xpath(dinamikFiyatXpathi))
+                                                    .getText();
+
+            if (satirdakiCategoryDegeri.equals("Furniture")){
+                System.out.println("Category'si Furniture olan urunun fiyati : "+ satirdakiFiyatDegeri);
+            }
+
+        }
 
         //10. Bir method olusturun, Test sayfasindan satir ve sutun verildiginde datayi yazdirsin
         //    ((//div[@role='trow'])[4]/div[@role='tdata'])[2]
+
+        printData(1,2); // Electronics
+        printData(2,3); // $399.00
+
+    }
+
+    public void printData(int satirNo, int sutunNo){
+
+        //    ((//div[@role='trow'])[   4   ]/div[@role='tdata'])[   2   ]
+
+        String dinamikXpath = "((//div[@role='trow'])[" + satirNo + "]/div[@role='tdata'])["+sutunNo+"]";
+
+        WebElement istenenWebelement = driver.findElement(By.xpath(dinamikXpath));
+
+        System.out.println(satirNo+".satir,"+sutunNo+".sutundaki data : " + istenenWebelement.getText());
+
     }
 }
